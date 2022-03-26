1a20e444a32d691c3abf54959ca7ccb283d05782
tmwilliamlin168 <8696482+tmwilliamlin168@users.noreply.github.com>
2022-03-27 01:58:34 +0800

    everything before result screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const text = 'The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. I know I\'m not very creative. By the way this code will probably be on GitHub if you are somehow interested. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.';
+const textTokens = text.split(' ');
+
 function App() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const listener = () => setProgress(Math.min(progress + 1, textTokens.length));
+    document.addEventListener('keyup', listener);
+
+    return () => document.removeEventListener('keyup', listener);
+  }, [progress]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="Header">Typer Trainer Beta</h1>
+      <div className="Type-box">
+        {textTokens.slice(0, progress).join(' ')}
+        <span className="Cursor">_</span>
+      </div>
     </div>
   );
 }
